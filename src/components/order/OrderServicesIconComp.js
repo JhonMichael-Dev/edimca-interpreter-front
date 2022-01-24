b1ea887780648393c9bfbbaae8c41f053775577a
yen <gustcas_29@hotmail.com>
2022-01-24 15:45:21 -0500

    WIP V1 (agregar fecha,asesor,hora de la orden

--- src/components/order/OrderServicesIconComp.js
+++ src/components/order/OrderServicesIconComp.js
@@ -60,11 +60,11 @@ export const OrderServicesIconComp = observer((props) => {
   Return
   */
     return (
-        <div className="card" style={{ width: "100px", textAlign: "center", justifyContent: "center" }} title={props.serviceType}>
+        <div className="card" style={{ width: "73px", textAlign: "center", justifyContent: "center" }} title={props.serviceType}>
             <i className="p-overlay-badge">
-                <img src={"/assets/images/serviceType_" + props.serviceType + ".png"} className="pos-edimca-button-noLabel" style={{ width: "40px", height: "40px" }}></img>
+                <img src={"/assets/images/serviceType_" + props.serviceType + ".png"} className="pos-edimca-button-noLabel" style={{ width: "30px", height: "30px" }}></img>
                 {props.badgeNumber !== null ? <Badge value={props.badgeNumber ? props.badgeNumber : 0} severity={props.badgeNumber && props.badgeNumber > 0 ? "warning" : "success"}></Badge> : ""}
-                <div style={{ fontSize: 10 }}> {props.serviceType}</div>
+                <div style={{ fontSize: 8 }}> {props.serviceType}</div>
             </i>
         </div>
     );
